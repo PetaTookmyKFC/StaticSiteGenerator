26573e7bd22a07240cd7dde10617bb86b0a2cc14
britishtrianglez <jackafoster1@gmail.com>
2023-04-17 17:00:52 +0100

    Updated to output in correct directory

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const path = require("path");
 const {Render} = require("./ServerRenderer");
 const chokidar = require("chokidar");
 
+exports.DevServer = (source, outputDirectory) => {
+    var bs = require("browser-sync").create();
+    
+    // .init starts the server
+    bs.init({
+        server: outputDirectory
+    });
+    
+    const watcherDirectory = source;
+
 // AutoreloadServer
-var bs = require("browser-sync").create();
-
-// .init starts the server
-bs.init({
-    server: "./Dist"
-});
-
-const watcherDirectory = path.join(__dirname, "src");
 
 let watcher = chokidar.watch(watcherDirectory);
 const log = console.log.bind(console);
@@ -25,9 +27,9 @@ watcher
             rendFile = fs.readFileSync(filepath).toString();
         }
         let nwPath = filepath.replace(watcherDirectory, "");
-        nwPath = path.join(__dirname, "Dist", nwPath);
+        nwPath = path.join(outputDirectory, nwPath);
         fs.writeFileSync(nwPath, rendFile);
-
+        console.log("Writing File ", nwPath);
     })
     .on('change', async (filepath) => {
         let rendFile;
@@ -40,23 +42,21 @@ watcher
         }
         console.log("UPDATED MENU");
         let nwPath = filepath.replace(watcherDirectory, "");
-        nwPath = path.join(__dirname, "Dist", nwPath);
+        nwPath = path.join(outputDirectory, nwPath);
         if (fs.existsSync(nwPath)) {
             fs.rmSync(nwPath);
         }
         fs.writeFileSync(nwPath, rendFile);
-
+        console.log("Writing File ", nwPath);
     })
     .on('unlink', async (filepath) => {
 
         let nwPath = filepath.replace(watcherDirectory, "");
-        nwPath = path.join(__dirname, "Dist", nwPath);
+        nwPath = path.join(outputDirectory, nwPath);
 
         if (fs.existsSync(nwPath)) {
             fs.rmSync(nwPath);
         }
-
-        bs.reload("*.html");
     });
 
 // Handle Directorys
@@ -66,19 +66,19 @@ watcher
         // path.replace
         let nwPath = filepath.replace(watcherDirectory, "");
         // New Directory Location
-        nwPath = path.join(__dirname, "Dist", nwPath);
+        nwPath = path.join(outputDirectory, nwPath);
         if (!fs.existsSync(nwPath)) {
             fs.mkdirSync(nwPath);
         }
     })
     .on('unlinkDir', filepath => {
         let nwPath = filepath.replace(watcherDirectory, "");
-        nwPath = path.join(__dirname, "Dist", nwPath);
+        nwPath = path.join(outputDirectory, nwPath);
 
         if (fs.existsSync(nwPath)) {
             fs.rmdirSync(nwPath);
         }
-
-        bs.reload("*.html");
     });
 
+
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+
+Example Script
+```js
+const path = require("path");
+const {DevServer} = require("./StaticSiteGenerator/index.js");
+
+
+DevServer(path.join(__dirname, "Prehnite"), path.join(__dirname,"Britishtrianglez.github.io"));
+
+```
