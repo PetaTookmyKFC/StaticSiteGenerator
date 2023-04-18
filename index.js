950da60952e6ccfb49f20156d6b13579e8567cda
britishtrianglez <jackafoster1@gmail.com>
2023-04-18 12:13:25 +0100

    Only triggered rendering once every 2 seconds

--- index.js
+++ index.js
@@ -21,12 +21,26 @@ exports.DevServer = (source, outputDirectory) => {
     function ScanDirectory(DirectoryLocation) {
         return new Promise(async (resolve, reject) => {
             try {
+                // console.log("Scanning directory : ", DirectoryLocation);
                 let directoryScan = await fs.promises.readdir(DirectoryLocation);
                 directoryScan.forEach(async (item) => {
                     if (item != ".git") {
                         if (fs.statSync(path.join(DirectoryLocation, item)).isDirectory()) {
+                            let curPath = path.join(DirectoryLocation, item);
+                            let nwPath = curPath.replace(watcherDirectory, "");
+                            nwPath = path.join(outputDirectory, nwPath);
+
+                            console.log("Scanning Directory ", curPath);
+
+                            if (!fs.existsSync(nwPath)) {
+                                fs.mkdirSync(nwPath);
+                            }
+
                             await ScanDirectory(path.join(DirectoryLocation, item));
                         } else {
+
+                            console.log("Rendering File ", path.join(DirectoryLocation, item));
+
                             await RenderFile(path.join(DirectoryLocation, item));
                         }
                     }
@@ -42,43 +56,51 @@ exports.DevServer = (source, outputDirectory) => {
     function RenderFile(RenderFile) {
         return new Promise(async (resolve, reject) => {
             let rendResult;
-            if (path.extname(RenderFile) == ".html") {
-                rendResult = await Render(RenderFile);
-            } else {
-                rendResult = await fs.readFileSync(RenderFile).toString();
+            try {
+                if (path.extname(RenderFile) == ".html") {
+                    rendResult = await Render(RenderFile);
+                } else {
+                    rendResult = await fs.readFileSync(RenderFile).toString();
+                }
+
+                let nwPath = RenderFile.replace(watcherDirectory, "");
+                nwPath = path.join(outputDirectory, nwPath);
+                fs.writeFileSync(nwPath, rendResult);
+            } catch (error) {
+                // console.log("DUBUGGER _TRIGGERED", error);
+                let nwPath = RenderFile.replace(watcherDirectory, "");
+                nwPath = path.join(outputDirectory, nwPath);
+                fs.writeFileSync(nwPath, error.toString());
             }
-
-            let nwPath = RenderFile.replace(watcherDirectory, "");
-            nwPath = path.join(outputDirectory, nwPath);
-            fs.writeFileSync(nwPath, rendResult);
-
             resolve();
         })
     }
-
     watcher
         .on('add', async (filepath) => {
-            await ScanDirectory(watcherDirectory);
-            bs.reload();
+            // console.log(" - Add Triggered");
+            TriggerReload();
         })
         .on('change', async (filepath) => {
-            await ScanDirectory(watcherDirectory);
-            bs.reload();
+            // console.log(" - Change Triggered");
+            TriggerReload();
         })
         .on('unlink', async (filepath) => {
+            // console.log(" - Unlink Triggered");
+
             let nwPath = filepath.replace(watcherDirectory, "");
             nwPath = path.join(outputDirectory, nwPath);
             if (fs.existsSync(nwPath)) {
                 fs.rmSync(nwPath);
             }
-            await ScanDirectory(watcherDirectory);
-            bs.reload();
+            TriggerReload();
         });
 
     // Handle Directorys
 
     watcher
-        .on('addDir', async ( filepath ) => {
+        .on('addDir', async (filepath) => {
+            // console.log(" - AddDir Triggered");
+
             // path.replace
             let nwPath = filepath.replace(watcherDirectory, "");
             // New Directory Location
@@ -86,17 +108,40 @@ exports.DevServer = (source, outputDirectory) => {
             if (!fs.existsSync(nwPath)) {
                 fs.mkdirSync(nwPath);
             }
-            await ScanDirectory(watcherDirectory);
-            bs.reload();
+            // await ScanDirectory(watcherDirectory);
+            // bs.reload();
         })
-        .on('unlinkDir', async ( filepath ) => {
+        .on('unlinkDir', async (filepath) => {
+            // console.log(" - RemDir Triggered");
+
+
             let nwPath = filepath.replace(watcherDirectory, "");
             nwPath = path.join(outputDirectory, nwPath);
             if (fs.existsSync(nwPath)) {
-                fs.rmdirSync(nwPath);
+                fs.rmdirSync(nwPath, { recursive: true, force: true });
             }
+            TriggerReload();
+        });
+    // ScanDirectory(watcherDirectory);
+
+    let ReloadScheduled = false;
+    async function TriggerReload() {
+        if (ReloadScheduled == false) {
+            ReloadScheduled = true;
+            console.clear();
+            console.log("Reloading");
+            await Delay();
             await ScanDirectory(watcherDirectory);
             bs.reload();
+            ReloadScheduled = false;
+        }
+    }
+    function Delay() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve();
+            }, 2000);
         });
-    ScanDirectory(watcherDirectory);
+    }
+    TriggerReload();
 }
